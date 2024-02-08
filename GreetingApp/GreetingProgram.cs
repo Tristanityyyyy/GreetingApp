44f3ebe9e2f0ea1247e8c31ed71c3ea676552f91
Labjata.315393 <Labjata.315393@LAB-A-PC29>
2024-02-08 08:28:32 +0800

    Add project files.

---
+++ GreetingApp/GreetingProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace GreetingApp
+{
+    class GreetingProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter the total number of your enrolled courses: ");
+
+            int enrolledCourse;
+            enrolledCourse = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the price of your favorite book: ");
+            double price;
+            price = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Total enrolled courses: " + enrolledCourse);
+            Console.WriteLine("Price of my favorite booke: " + price);
+
+            Console.Write("\n Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
